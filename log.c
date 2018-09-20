3cc6fbb826e182be6c376c8793e0fcca7932c32e
Dr-Noob <peibolms@gmail.com>
2018-09-20 23:56:01 +0200

    Fix bug when missing UA

--- log.c
+++ log.c
@@ -16,17 +16,18 @@ void print_log(struct http_request* req) {
   assert(log_file != NULL);
 
   //May be NULL
-  if(req->ua == NULL)
-    req->ua = "???";
+  char* ua = req->ua;
+  if(ua == NULL)
+    ua = "???";
 
   int fd = open(log_file, O_CREAT| O_WRONLY | O_APPEND, 0644);
 
   if (fd == -1)
     perror("print_log: open");
   else {
-    int size = strlen(req->ip)+strlen(req->ua)+strlen(req->method)+strlen(req->resource)+2+1+2+2;
+    int size = strlen(req->ip)+strlen(ua)+strlen(req->method)+strlen(req->resource)+2+1+2+2;
     char message[(sizeof(char)*(size+1))];
-    snprintf(message, size+1, "[%s]-[%s]%s:%s\n", req->ip,req->ua,req->method,req->resource);
+    snprintf(message, size+1, "[%s]-[%s]%s:%s\n", req->ip,ua,req->method,req->resource);
     write_all(fd,message,size);
     close(fd);
   }
